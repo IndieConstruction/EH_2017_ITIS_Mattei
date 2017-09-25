eafd6b7187af7a53db0b19ced393c0afaa5019bb
Brago <bragolab@gmail.com>
2017-09-25 18:21:38 +0200

    Respawn Mario con vite a 0; Enemy di varie tipologie. Il Player uccide i nemici se gli salta in testa.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,21 +6,12 @@ public class Enemy : MonoBehaviour {
 
 	public int Life;
 	public int Damage;
-	public float MovementeSpeed;
-	public Transform PatrolPoint1;
-	public Transform PatrolPoint2;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-	public void MoveToPatrolPosition(){
-	
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.gameObject.tag == "Player") {
+			Destroy (this.gameObject);
+		}
 	}
 
 }
